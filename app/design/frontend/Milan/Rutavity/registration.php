f4ec0adf013f0259bde94d1754d86ce6d19627fa
Richerman <richerman@ip-192-168-14-222.ec2.internal>
2022-07-11 12:42:10 -0400

    BM-49: Create basic custom theme, install fonts, assets

---
+++ app/design/frontend/Milan/Rutavity/registration.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Rutavity theme for Bicicletas Milan by Wagento
+ * @package Milan_Rutavity
+ * @author Rudie Wang <rudi.wang@wagento.com>
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::THEME,
+    'frontend/Milan/Rutavity',
+    __DIR__
+);
