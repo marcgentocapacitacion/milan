26472ad7f79ca32bacd596c937d63dc6f514fee2
Rulo <bohemiorulo@gmail.com>
2022-08-11 18:21:17 -0400

    Develop the Product Category Page

---
+++ app/code/Wagento/SelectCustom/registration.php
@@ -0,0 +1,9 @@
+<?php
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(
+    ComponentRegistrar::MODULE, 'Wagento_SelectCustom',
+    __DIR__
+);
+
---
+++ app/code/Wagento/SelectCustom/view/frontend/requirejs-config.js
@@ -0,0 +1,5 @@
+var config = {
+    paths: {
+        'select2': 'Wagento_SelectCustom/js/select2'
+    }
+};
