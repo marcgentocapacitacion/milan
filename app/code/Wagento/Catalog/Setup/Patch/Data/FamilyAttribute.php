651388068b24ae6e62ca45c9de2525ab3ed506d8
Tiago Tescaro <tiago.tescaro@wagento.com>
2023-01-09 16:58:23 +0000

    add new field image product family

--- app/code/Wagento/Catalog/Setup/Patch/Data/FamilyAttribute.php
+++ app/code/Wagento/Catalog/Setup/Patch/Data/FamilyAttribute.php
@@ -2,7 +2,7 @@
 
 namespace Wagento\Catalog\Setup\Patch\Data;
 
-use Magento\Catalog\Model\Product;
+use Magento\Catalog\Model\Category;
 use Magento\Catalog\Setup\CategorySetup;
 use Magento\Catalog\Setup\CategorySetupFactory;
 use Magento\Framework\Setup\ModuleDataSetupInterface;
@@ -59,26 +59,22 @@ class FamilyAttribute implements \Magento\Framework\Setup\Patch\DataPatchInterfa
         /** @var CategorySetup $eavSetup */
         $eavSetup = $this->categorySetupFactory->create(['setup' => $this->moduleDataSetup]);
         $eavSetup->addAttribute(
-            Product::ENTITY,
-            'product_family',
+            Category::ENTITY,
+            'product_family_image',
             [
-                'type' => 'int',
-                'label' => 'Product Family',
-                'input' => 'select',
-                "frontend_input" => "swatch_visual",
-                'swatch_input_type' => 'visual',
-                'used_in_product_listing' => '1',
+                'type' => 'varchar',
+                'label' => 'Product Family Image',
+                "input" => "image",
                 'required' => false,
                 'sort_order' => 150,
-                'global' => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_GLOBAL,
-                'group' => 'General Information',
+                'global' => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_STORE,
+                'group' => 'content',
+                'backend' => \Magento\Catalog\Model\Category\Attribute\Backend\Image::class,
+                'system' => true,
                 'is_used_in_grid' => true,
                 'is_visible_in_grid' => true,
                 'is_filterable_in_grid' => true,
                 'is_visible' => true,
-                'filterable' => 2,
-                'filterable_in_search' => true,
-                'used_for_promo_rules' => true,
                 'is_html_allowed_on_front' => true,
                 'used_for_sort_by' => true,
                 'is_user_defined' => true
