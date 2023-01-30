fa6d170ff9c15c7506d7da7129261ea41f2cb343
Tiago Tescaro <tiago.tescaro@wagento.com>
2023-01-30 20:23:57 +0000

    Add new field in import

--- app/code/Wagento/ImportData/Model/ProductDescription/Tabs/AbstractTabs.php
+++ app/code/Wagento/ImportData/Model/ProductDescription/Tabs/AbstractTabs.php
@@ -31,6 +31,7 @@ class AbstractTabs
                         data-background-images="{}"
                         data-element="main"
                         data-pb-style="TYH99RM">';
+        $html .= $this->getField($row);
         $html .= '</div>';
         $html .= '<div  class="pagebuilder-column"
                         data-content-type="column"
@@ -38,6 +39,38 @@ class AbstractTabs
                         data-background-images="{}"
                         data-element="main"
                         data-pb-style="X6X8P03">';
+        $html .= $this->getField($row);
+        $html .= '</div>';
+        $html .= '</div>';
+        $html .= '</div>';
+        return $html;
+    }
+
+    /**
+     * @param array $row
+     *
+     * @return string
+     */
+    public function getOneColumn(array $row): string
+    {
+        $html = '<div   class="pagebuilder-column-group"
+                        data-background-images="{}"
+                        data-content-type="column-group"
+                        data-appearance="default"
+                        data-grid-size="12"
+                        data-element="main"
+                        data-pb-style="AP3X06Y">';
+        $html .= '<div  class="pagebuilder-column-line"
+                        data-content-type="column-line"
+                        data-element="main"
+                        data-pb-style="JLXF9GK">';
+        $html .= '<div  class="pagebuilder-column"
+                        data-content-type="column"
+                        data-appearance="align-center"
+                        data-background-images="{}"
+                        data-element="main"
+                        data-pb-style="ONT9N6R">';
+        $html .= $this->getField($row);
         $html .= '</div>';
         $html .= '</div>';
         $html .= '</div>';
@@ -55,8 +88,29 @@ class AbstractTabs
             'image' => $this->getImage($row['data1'] ?? ''),
             'text' => $this->getText($row['data1'] ?? ''),
             'link' => $this->getALink($row['data1'] ?? 'Click here', $row['data2'] ?? ''),
+            'video_youtube' => $this->getVideoYoutube($row['data1'] ?? ''),
             default => $row['data1'] ?? ''
         };
+    }
+
+    /**
+     * @param string $url
+     *
+     * @return string
+     */
+    public function getVideoYoutube(string $url): string
+    {
+        return '<div data-content-type="video" data-appearance="default" data-element="main" data-pb-style="L33T0U4">
+                <div class="pagebuilder-video-inner" data-element="inner" data-pb-style="T81C5D2">
+                    <div class="pagebuilder-video-wrapper" data-element="wrapper">
+                        <div class="pagebuilder-video-container">
+                            <iframe frameborder="0" allowfullscreen=""
+                                    src="' . $url . '"
+                                    autoplay="true" muted="true" data-element="video"></iframe>
+                        </div>
+                    </div>
+                </div>
+            </div>';
     }
 
     /**
@@ -175,6 +229,40 @@ class AbstractTabs
         #html-body [data-pb-style=F4Q0BIE] {
             margin-left: 15px;
             margin-right: 15px
+        }
+
+        #html-body [data-pb-style=T81C5D2] {
+            max-width: 1000px
+        }
+
+        #html-body [data-pb-style=L33T0U4] {
+            text-align: center;
+            margin-left: 10px;
+            margin-right: 10px
+        }
+        #html-body [data-pb-style=AP3X06Y] {
+            background-position: left top;
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-attachment: scroll;
+            align-self: stretch
+        }
+        #html-body [data-pb-style=JLXF9GK] {
+            display: flex;
+            width: 100%
+        }
+        #html-body [data-pb-style=ONT9N6R] {
+            justify-content: center;
+            display: flex;
+            flex-direction: column;
+            background-position: left top;
+            background-size: cover;
+            background-repeat: no-repeat;
+            background-attachment: scroll;
+            width: 100%;
+            margin-left: 10px;
+            margin-right: 10px;
+            align-self: center
         }
         ";
     }
