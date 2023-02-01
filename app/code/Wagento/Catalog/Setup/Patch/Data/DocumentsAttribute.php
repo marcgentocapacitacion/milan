f9cb71c71bcc4c7589fcfb7859044123ebd72732
Tiago Tescaro <tiago.tescaro@wagento.com>
2023-02-01 18:27:43 +0000

    Add tab documents and attribute

---
+++ app/code/Wagento/Catalog/Setup/Patch/Data/DocumentsAttribute.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace Wagento\Catalog\Setup\Patch\Data;
+
+use Magento\Catalog\Model\Category;
+use Magento\Catalog\Model\Product;
+use Magento\Catalog\Setup\CategorySetup;
+use Magento\Catalog\Setup\CategorySetupFactory;
+use Magento\Framework\Setup\ModuleDataSetupInterface;
+
+/**
+ * Class DocumentsAttribute
+ */
+class DocumentsAttribute implements \Magento\Framework\Setup\Patch\DataPatchInterface
+{
+    /**
+     * @var ModuleDataSetupInterface
+     */
+    private $moduleDataSetup;
+
+    /**
+     * @var CategorySetupFactory
+     */
+    private $categorySetupFactory;
+
+    /**
+     * PatchInitial constructor.
+     * @param ModuleDataSetupInterface $moduleDataSetup
+     * @param CategorySetupFactory $categorySetupFactory
+     */
+    public function __construct(
+        ModuleDataSetupInterface $moduleDataSetup,
+        CategorySetupFactory $categorySetupFactory
+    ) {
+        $this->moduleDataSetup = $moduleDataSetup;
+        $this->categorySetupFactory = $categorySetupFactory;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public static function getDependencies()
+    {
+        return [];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function getAliases()
+    {
+        return [];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function apply()
+    {
+        /** @var CategorySetup $eavSetup */
+        $eavSetup = $this->categorySetupFactory->create(['setup' => $this->moduleDataSetup]);
+        $eavSetup->addAttribute(
+            Product::ENTITY,
+            'documents',
+            [
+                'type' => 'varchar',
+                'label' => 'Documents',
+                "input" => "fileUploader",
+                'required' => false,
+                'sort_order' => 150,
+                'global' => \Magento\Eav\Model\Entity\Attribute\ScopedAttributeInterface::SCOPE_STORE,
+                'group' => 'content',
+                'system' => true,
+                'is_used_in_grid' => true,
+                'is_visible_in_grid' => true,
+                'is_filterable_in_grid' => true,
+                'is_visible' => true,
+                'is_html_allowed_on_front' => true,
+                'used_for_sort_by' => true,
+                'is_user_defined' => true
+            ]
+        );
+    }
+}
