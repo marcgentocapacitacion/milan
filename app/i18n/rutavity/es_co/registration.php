00d2f43aa7620d73f1f5541e6b85007fcc823f6d
Rudie Wang <rudi.wang@wagento.com>
2023-02-13 16:22:21 -0500

    Add customt translation package [BM-146]

---
+++ app/i18n/rutavity/es_co/registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Ruvativy custom translate language
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
+    'rutavity_es_CO',
+    __DIR__
+);
