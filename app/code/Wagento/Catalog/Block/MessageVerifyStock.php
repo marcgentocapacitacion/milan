f18d2938e2a1d767b54954e4ad6ae8e75765fd48
Tiago Tescaro <tiago.tescaro@wagento.com>
2023-03-18 01:25:23 +0000

    Fix problem with message in catalog view

--- app/code/Wagento/Catalog/Block/MessageVerifyStock.php
+++ app/code/Wagento/Catalog/Block/MessageVerifyStock.php
@@ -2,11 +2,36 @@
 
 namespace Wagento\Catalog\Block;
 
+use Magento\Customer\Model\Session;
+use Magento\Framework\View\Element\Template;
+
 /**
  * Class MessageVerifyStock
  */
 class MessageVerifyStock extends \Magento\Framework\View\Element\Template
 {
+    /**
+     * @var Session
+     */
+    protected Session $session;
+
+    public function __construct(
+        Template\Context $context,
+        Session $session,
+        array $data = []
+    ) {
+        parent::__construct($context, $data);
+        $this->session = $session;
+    }
+
+    /**
+     * @return bool
+     */
+    public function isLoggedIn(): bool
+    {
+        return $this->session->isLoggedIn() ?? false;
+    }
+
     /**
      * @return string
      */
